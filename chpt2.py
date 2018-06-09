0adfd41ddd15448e0fc0696c2135d6bf47b6a691
davidweintraub@gmail.com <muffinman1>
2018-06-09 10:25:30 -0600

    Miranda is lame

--- chpt2.py
+++ chpt2.py
@@ -12,3 +12,5 @@ fmt = '{:15} | {:9.4f} | {:9.4f}'
 for name, cc, pop, (latitude, longitude) in metro_areas:
     if longitude <= 0:
         print(fmt.format(name, latitude, longitude))
+
+print ("Miranda is super lame")
